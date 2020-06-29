6419a7e167dfdd13f910e765d65630003b68bebc
PanGeun <lifebook0809@gmail.com>
2020-06-29 11:41:16 +0900

    [add] goorm_training1

---
+++ week1/goorm_training1.py
@@ -0,0 +1,12 @@
+hello = input()
+number = input()
+
+print("숫자를 1개 입력해주세요: " + number)
+
+input_float = 2.5
+input_integer = 100
+input_char = 'c'
+input_string = "Hello hufs"
+
+
+print(input_float, input_integer, input_char, input_string)
