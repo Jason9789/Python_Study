07ea06767c7d3493556c1054d66b65159a539aaf
PanGeun <lifebook0809@gmail.com>
2020-06-30 09:38:40 +0900

    [add] print string_type

---
+++ week3/GT2_string_type.py
@@ -0,0 +1,19 @@
+# He said "I love you" 를 출력해보자.
+
+str1 = 'He said "I love you"'
+str2 = "It's so beautiful"
+str3 = "My name is 'goorm'"
+str4 = """My name is "goorm" """
+str5 = '''Is's an apple'''
+
+# 3개의 따옴표를 사용하면 개행도 실행됨.
+str6 = '''It's
+an apple'''
+
+print(str1)
+print(str2)
+print(str3)
+print(str4)
+print(str5)
+print(str6)
+
