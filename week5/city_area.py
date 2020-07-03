0ef1862ebf5634b6e7921e1c1594114ceb8a973e
PanGeun <lifebook0809@gmail.com>
2020-07-03 19:15:43 +0900

    [add] week5 homework

---
+++ week5/city_area.py
@@ -0,0 +1,15 @@
+# 도시 면적 출력
+
+city = input("도시 면적 : ")
+
+if city == "서울":
+    print(f"{city} : 605km²")
+
+elif city == "파리":
+    print(f"{city} : 105km²")
+
+elif city == "애든버러":
+    print(f"{city} : 264km²")
+
+else:
+    print("Sorry")
