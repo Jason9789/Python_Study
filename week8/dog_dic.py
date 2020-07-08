2bed81a2f9be99ee34d6c6d2cd34616ca35661eb
PanGeun <lifebook0809@gmail.com>
2020-07-08 22:12:55 +0900

    [edit] some code

--- week8/dog_dic.py
+++ week8/dog_dic.py
@@ -1,20 +1,20 @@
 # 강아지 딕셔너리 생성
-dog = []
-dog_list = {}
+dog_list = []
+dog = {}
 
 for num in range(3):
-    dog_list['name'] = input("강아지 이름 : ")
-    dog_list['age'] = int(input("강아지 나이 : "))
-    dog_list['king'] = input("강아지 종류 : ")
-    dog.append(dict(dog_list))
+    dog['name'] = input("강아지 이름 : ")
+    dog['age'] = int(input("강아지 나이 : "))
+    dog['king'] = input("강아지 종류 : ")
+    dog_list.append(dict(dog))
 
 print("리스트 내용")
-for num in range(3):
-    print(f"{num} 번째 : ", dog[num])
+for num in range(len(dog_list)):
+    print(f"{num} 번째 : ", dog_list[num])
 
 
 print("나이가 3살 이상인 강아지")
-for i in range(3):
-    if dog[i]['age'] >= 3:
-        print(dog[i])
+for dog in dog_list:
+    if dog['age'] >= 3:
+        print(dog)
 
--- week8/set.py
+++ week8/set.py
@@ -1,6 +1,6 @@
 
-set1 = set(('x', 'b', 'g', 'd', 'q'))
-set2 = set(('b', 'w', 'g', 'q', 'o'))
+set1 = {'x', 'b', 'g', 'd', 'q'}
+set2 = {'b', 'w', 'g', 'q', 'o'}
 
 print("set1 = ", set1)
 print("set2 = ", set2)
@@ -16,16 +16,16 @@ char = input("영문자 한자 입력 : ")
 
 if char in set1:
     print("중복된 입력입니다.")
-    print("set1 = ", set1)
 else:
     set1.add(char)
-    print("set1 = ", set1)
+
+print("set1 = ", set1)
 
 delChar = input("영문자 한자 입력 : ")
 if delChar in set2:
     set2.remove(delChar)
-    print("set2 = ", set2)
 
 else:
     print("삭제할 문자가 없습니다.")
-    print("set2 = ", set2)
+
+print("set2 = ", set2)
