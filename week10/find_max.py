678a30473e12827d552e3bea00f90c832d2d28d9
PanGeun <lifebook0809@gmail.com>
2020-07-11 01:53:32 +0900

    [edit] find_max

--- week10/find_max.py
+++ week10/find_max.py
@@ -13,3 +13,30 @@
 # find_max(num1, num2) 실행하여 결과값 찾고 출력
 # 3,4,5 개의 매개변수를 가지고 실행하여 출력
 
+num_list = []
+
+
+def find_max(*values):
+    max = 0
+    str = ''
+
+    n = len(values)
+    for i in range(n):
+        if max < values[i]:
+            max = values[i]
+        print("%d, " % values[i], end='')
+    print("max = %d" % max)
+
+
+for i in range(5):
+    num = int(input("정수 입력 : "))
+    num_list.append(num)
+
+
+find_max()
+find_max(num_list[0])
+find_max(num_list[0], num_list[1])
+find_max(num_list[0], num_list[1], num_list[2])
+find_max(num_list[0], num_list[1], num_list[2], num_list[3])
+find_max(num_list[0], num_list[1], num_list[2], num_list[3], num_list[4])
+
