d6452de68f131dda9878fe240cafaaf4eec67ade
PanGeun <lifebook0809@gmail.com>
2020-07-10 23:23:34 +0900

    [edit] some code

--- week10/print_char.py
+++ week10/print_char.py
@@ -4,25 +4,19 @@
 # 매개변수1 : 출력할 특수 문자
 # 매개변수2 : 출력할 라인 수
 
-# SC = special Character (특수 문자)
+# sc = special Character (특수 문자)
 # num = 라인 수
-def print_char(SC, num):
+
+
+def print_char(sc, num):
     for i in range(num):
         for j in range(num - i):
             print("=", end='')
         for j in range(i + 1):
-            print(SC, end='')
+            print(sc, end='')
         print()
 
 
-print_char("&", 5)
+print_char("%", 5)
 print_char("&", 10)
 
-# chara = input("특수 : ")
-# num = int(input("라인 : "))
-# for i in range(num + 1):
-#     for j in range(num - i + 1):
-#         print("=", end='')
-#     for j in range(i):
-#         print(chara, end='')
-#     print()
