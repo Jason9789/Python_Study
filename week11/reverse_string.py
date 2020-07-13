81e5522c9dd67e7c3212549668d3266d637d4cd9
PanGeun <lifebook0809@gmail.com>
2020-07-13 16:47:35 +0900

    [add] reverse string

---
+++ week11/reverse_string.py
@@ -0,0 +1,13 @@
+def reverse_string(original, i):
+    if -len(original) > i:
+        return
+
+    else:
+        print(original[i], end='')
+        reverse_string(original, i-1)
+
+
+
+
+original = input("문자열 입력 : ")
+reverse_string(original, -1)
