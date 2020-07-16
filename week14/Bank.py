c92695d318a02f8377a0335ad658aa82edbf5a1e
PanGeun <lifebook0809@gmail.com>
2020-07-16 18:01:30 +0900

    [edit] some code

--- week14/Bank.py
+++ week14/Bank.py
@@ -1,7 +1,7 @@
 
 
 class BankAccount:
-    __rate = 0.05
+    rate = 0.05
 
     def __init__(self, name, balance=0):
         self.__name = name
@@ -27,7 +27,7 @@ class BankAccount:
         return self.__balance
 
     def calcuInterest(self):
-        tmp = self.__balance * self.__rate
+        tmp = self.__balance * BankAccount.rate
         self.__balance += int(tmp)
 
 
@@ -40,26 +40,31 @@ person1.disp()
 person2.disp()
 print("----------------------------")
 
+
 # 김철수 150000 입금, 50000 출금
 print(f"{person1.get_name()} 150,000 입금 / 50,000 출금")
 person1.deposit(150000)
 print(f"{person1.get_name()} 잔액 : {person1.get_balance()}")
+
 if person1.withdraw(50000) == 1:
     print(f"50000원 출금 잔액 : {person1.get_balance()}")
 elif person1.withdraw(50000) == -1:
     print("잔액이 부족")
 print("----------------------------")
 
+
 # 이영희 50000 입금, 100000 출금
 print(f"{person2.get_name()} 50,000 입금 / 100,000 출금")
 person2.deposit(50000)
-print(f"{person2.get_name()} 잔액 : {person1.get_balance()}")
+print(f"{person2.get_name()} 잔액 : {person2.get_balance()}")
+
 if person2.withdraw(100000) == 1:
-    print(f"50000원 출금 잔액 : {person1.get_balance()}")
+    print(f"50000원 출금 잔액 : {person2.get_balance()}")
 elif person2.withdraw(100000) == -1:
     print("잔액이 부족")
     print(f"{person2.get_name()} 잔액 : {person2.get_balance()}")
 print("----------------------------")
+
 
 # 이자 계산 전
 print("이자 계산 전")
